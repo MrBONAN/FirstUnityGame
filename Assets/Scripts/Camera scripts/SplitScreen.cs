5a077af4e245647446a4f2ca20a17e5f8246e78d
MrBONAN <taleshatrycob@gmail.com>
2024-04-28 15:58:57 +0500

    Добавлена возможность отключить split режим

--- Assets/Scripts/Camera scripts/SplitScreen.cs
+++ Assets/Scripts/Camera scripts/SplitScreen.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class SplitScrin : MonoBehaviour
 {
+    public bool splitScreen = true;
+
     public Camera camera1;
     public Camera camera2;
 
@@ -11,7 +13,8 @@ public class SplitScrin : MonoBehaviour
 
     public float cameraSpeed = 15f;
     public float convergenceSpeed = 5f;
-    private float halfWidth, halfHeight; // половина ширины и высота камеры в юнитах
+
+    public float halfWidth, halfHeight; // половина ширины и высота камеры в юнитах
     public float orthographicSize = 5f;
     private float zPos = -10f;
 
@@ -24,6 +27,17 @@ public class SplitScrin : MonoBehaviour
 
     public void Start()
     {
+        if (splitScreen)
+            InitializeSplitModeSettings();
+        else
+            InitializeMonoCameraSettings();
+    }
+
+    private void InitializeSplitModeSettings()
+    {
+        camera1.rect = new Rect(0, 0, 0.5f, 1);
+        camera2.rect = new Rect(0.5f, 0, 0.5f, 1);
+        
         halfHeight = orthographicSize / 2;
         halfWidth = orthographicSize * camera1.pixelWidth / camera1.pixelHeight;
         xMaxPlayersDistance = halfWidth * 2.5f;
@@ -31,17 +45,38 @@ public class SplitScrin : MonoBehaviour
         dWidth = halfWidth - 3;
         dHeight = halfHeight - 1;
 
-        camera1.rect = new Rect(0, 0, 0.5f, 1);
-        camera2.rect = new Rect(0.5f, 0, 0.5f, 1);
+
         camera1.orthographicSize = orthographicSize;
         camera2.orthographicSize = orthographicSize;
-        camera1.transform.position = new Vector3(-halfWidth, 0, zPos);
-        camera2.transform.position = new Vector3(halfWidth, 0, zPos);
+
+        var playerCenter = (player1.position + player2.position) / 2;
+        camera1.transform.position = playerCenter + new Vector3(-halfWidth, 0, zPos);
+        camera2.transform.position = playerCenter + new Vector3(halfWidth, 0, zPos);
+    }
+
+    private void InitializeMonoCameraSettings()
+    {
+        camera1.rect = new Rect(0, 0, 1, 1);
+        
+        halfHeight = orthographicSize;
+        halfWidth = orthographicSize * camera1.pixelWidth / camera1.pixelHeight;
+        dWidth = halfWidth / 2;
+        dHeight = halfHeight / 2;
+        
+        camera1.orthographicSize = orthographicSize;
+        camera1.transform.position = player1.position + new Vector3(0, 0, zPos);
+        
+        camera2.enabled = false;
     }
 
 
     public void FixedUpdate()
     {
+        if (!splitScreen)
+        {
+            camera1.transform.position += GetCameraMoveDirection(camera1, player1.position) * cameraSpeed;
+            return;
+        }
         var playersDistance = player1.position - player2.position;
         if (Math.Abs(playersDistance.x) < xMaxPlayersDistance &&
             Math.Abs(playersDistance.y) < yMaxPlayersDistance)
@@ -52,23 +87,11 @@ public class SplitScrin : MonoBehaviour
 
     private void MoveConnectedCameras()
     {
-        var cp1 = camera1.transform.position;
-        var cp2 = camera2.transform.position;
         var playersCenter = (player1.position + player2.position) / 2;
-        var camerasCenter = (cp1 + cp2 - new Vector3(0, 0, 2 * zPos)) / 2;
 
         var targetPos1 = new Vector3(playersCenter.x - halfWidth, playersCenter.y, zPos);
         var targetPos2 = new Vector3(playersCenter.x + halfWidth, playersCenter.y, zPos);
 
-        // if ((cp1 - player1.position).sqrMagnitude > (cp1 - player2.position).sqrMagnitude)
-        //     (targetPos1, targetPos2) = (targetPos2, targetPos1);
-
-        // if ((playersCenter - camerasCenter).sqrMagnitude < 0.05f)
-        // {
-        //     camera1.transform.position = targetPos1;
-        //     camera2.transform.position = targetPos2;
-        // }
-        // else
         camera1.transform.position +=
             (targetPos1 - camera1.transform.position) * (Time.fixedDeltaTime * convergenceSpeed);
         camera2.transform.position +=
@@ -81,7 +104,7 @@ public class SplitScrin : MonoBehaviour
         var target2 = player2.position;
         if ((camera1.transform.position - target1).sqrMagnitude > (camera1.transform.position - target2).sqrMagnitude)
             (target1, target2) = (target2, target1);
-        
+
         camera1.transform.position += GetCameraMoveDirection(camera1, target1) * cameraSpeed;
         camera2.transform.position += GetCameraMoveDirection(camera2, target2) * cameraSpeed;
     }
@@ -89,8 +112,8 @@ public class SplitScrin : MonoBehaviour
     private Vector3 GetCameraMoveDirection(Camera camera, Vector3 target)
     {
         var dxy = target - camera.transform.position;
-        var dx = Math.Max(Math.Abs(dxy.x) - dWidth / 2, 0f) * Math.Sign(dxy.x) / 4 * Time.fixedDeltaTime;
-        var dy = Math.Max(Math.Abs(dxy.y) - dHeight / 2, 0f) * Math.Sign(dxy.y) / 4 * Time.fixedDeltaTime;
+        var dx = Math.Max(Math.Abs(dxy.x) - dWidth, 0f) * Math.Sign(dxy.x) / 4 * Time.fixedDeltaTime;
+        var dy = Math.Max(Math.Abs(dxy.y) - dHeight, 0f) * Math.Sign(dxy.y) / 4 * Time.fixedDeltaTime;
         return new Vector3(dx, dy);
     }
 }
